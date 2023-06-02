a4c0ee862cd78f6dc27fb930c67c09da7ea2a1d9
alfian.yuska <alfian.yuska@phiraka.com>
2023-06-02 14:37:42 +0700

    Add project files.

---
+++ RazorPages/Data/RazorPagesDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPages.Models.Domain;
+
+namespace RazorPages.Data
+{
+    public class RazorPagesDemoDbContext : DbContext
+    {
+        public RazorPagesDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
